d97c867befa8da00151bfe3b035540c380d5806b
MNoori-coder <noorimohammad730@gmail.com>
2023-11-21 13:55:02 +0330

    question_category CRUD completed!

--- question_category/handlers.go
+++ question_category/handlers.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func ListQuestionCategory(w http.ResponseWriter, r *http.Request) {
@@ -42,6 +44,151 @@ func ListQuestionCategory(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	response := map[string]interface{}{
 		"data":    question_categories,
+		"success": true,
+	}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Error encoding response to JSON\n", http.StatusInternalServerError)
+		return
+	}
+}
+
+func CreateQuestionCategory(w http.ResponseWriter, r *http.Request) {
+	var question_category SchemaQuestionCategory
+	err := json.NewDecoder(r.Body).Decode(&question_category)
+	if err != nil {
+		http.Error(w, "Error decoding request body\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	db, err := sql.Open("sqlite3", "my_db.db")
+	if err != nil {
+		http.Error(w, "Can not connect to db\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer db.Close()
+
+	_, err = db.Exec("INSERT INTO question_category (name, content) VALUES (?, ?)", question_category.Name, question_category.Content)
+	if err != nil {
+		http.Error(w, "Error executing insert query\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	response := map[string]interface{}{
+		"success": true,
+	}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Error encoding response to JSON\n", http.StatusInternalServerError)
+		return
+	}
+}
+
+func UpdateQuestionCategory(w http.ResponseWriter, r *http.Request) {
+	vars := strings.Split(strings.TrimSuffix(r.URL.Path, "/"), "/")
+	question_category_IDStr := vars[len(vars)-1]
+
+	question_category_ID, err := strconv.Atoi(question_category_IDStr)
+	if err != nil {
+		http.Error(w, "Invalid ticket ID\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var question_category SchemaQuestionCategory
+	err = json.NewDecoder(r.Body).Decode(&question_category)
+	if err != nil {
+		http.Error(w, "Error decoding request body\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	db, err := sql.Open("sqlite3", "my_db.db")
+	if err != nil {
+		http.Error(w, "Can not connect to db\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer db.Close()
+
+	_, err = db.Exec("UPDATE question_category SET name=?, content=? WHERE id=?", question_category.Name, question_category.Content, question_category_ID)
+	if err != nil {
+		http.Error(w, "Error executing update query\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	response := map[string]interface{}{
+		"success": true,
+	}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Error encoding response to JSON\n", http.StatusInternalServerError)
+		return
+	}
+}
+
+func DeleteQuestionCategory(w http.ResponseWriter, r *http.Request) {
+	vars := strings.Split(strings.TrimSuffix(r.URL.Path, "/"), "/")
+	question_categoryIDStr := vars[len(vars)-1]
+
+	question_categoryID, err := strconv.Atoi(question_categoryIDStr)
+	if err != nil {
+		http.Error(w, "Invalid ticket ID\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	db, err := sql.Open("sqlite3", "my_db.db")
+	if err != nil {
+		http.Error(w, "Can not connect to db\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer db.Close()
+
+	_, err = db.Exec("DELETE FROM question_category WHERE id=?", question_categoryID)
+	if err != nil {
+		http.Error(w, "Error executing delete query\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	response := map[string]interface{}{
+		"success": true,
+	}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Error encoding response to JSON\n", http.StatusInternalServerError)
+		return
+	}
+}
+
+func RetrieveQuestionCategory(w http.ResponseWriter, r *http.Request) {
+	vars := strings.Split(strings.TrimSuffix(r.URL.Path, "/"), "/")
+	question_category_IDStr := vars[len(vars)-1]
+
+	question_category_ID, err := strconv.Atoi(question_category_IDStr)
+	if err != nil {
+		http.Error(w, "Invalid ticket ID\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	db, err := sql.Open("sqlite3", "my_db.db")
+	if err != nil {
+		http.Error(w, "Can not connect to db\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer db.Close()
+
+	row := db.QueryRow("SELECT * FROM question_category WHERE id=?", question_category_ID)
+
+	var question_category SchemaQuestionCategory
+	err = row.Scan(&question_category.ID, &question_category.Name, &question_category.Content)
+	if err != nil {
+		http.Error(w, "Error retrieving ticket\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	response := map[string]interface{}{
+		"data":    question_category,
 		"success": true,
 	}
 
--- question_category/routers.go
+++ question_category/routers.go
@@ -12,4 +12,31 @@ func QuestionCategoryRouters(mux *http.ServeMux) {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
 	})
+
+	// POST
+	mux.HandleFunc("/question_category/insert/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			CreateQuestionCategory(w, r)
+		} else {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	// PUT
+	mux.HandleFunc("/question_category/update/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPut {
+			UpdateQuestionCategory(w, r)
+		} else {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	// DELETE
+	mux.HandleFunc("/question_category/delete/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodDelete {
+			DeleteQuestionCategory(w, r)
+		} else {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
 }
