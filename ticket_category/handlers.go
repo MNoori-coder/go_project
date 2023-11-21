2c8c8dff1ec681cc7cd2467d284ceb15559de26f
MNoori-coder <noorimohammad730@gmail.com>
2023-11-21 12:11:49 +0330

    ticket_category Create implemented!

--- ticket_category/handlers.go
+++ ticket_category/handlers.go
@@ -50,3 +50,35 @@ func ListTicketCategory(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+func CreateTicketCategory(w http.ResponseWriter, r *http.Request) {
+	var ticket_category SchemaTicketCategory
+	err := json.NewDecoder(r.Body).Decode(&ticket_category)
+	if err != nil {
+		http.Error(w, "Error decoding request body\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	db, err := sql.Open("sqlite3", "my_db.db")
+	if err != nil {
+		http.Error(w, "Can not connect to db\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer db.Close()
+
+	_, err = db.Exec("INSERT INTO ticket_category (name, content) VALUES (?, ?)", ticket_category.Name, ticket_category.Content)
+	if err != nil {
+		http.Error(w, "Error executing insert query\n"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	response := map[string]interface{}{
+		"success": true,
+	}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Error encoding response to JSON\n", http.StatusInternalServerError)
+		return
+	}
+}
--- ticket_category/routers.go
+++ ticket_category/routers.go
@@ -15,4 +15,13 @@ func TicketCategoryRouters(mux *http.ServeMux) {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
 	})
+
+	// POST
+	mux.HandleFunc("/ticket_category/insert/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			CreateTicketCategory(w, r)
+		} else {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
 }
